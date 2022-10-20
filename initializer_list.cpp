a749a196ab8c97d53385656fde889fe9734e3b11
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-10-20 03:08:27 -0300

    Add initializer_list example

---
+++ initializer_list.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include <initializer_list>
+
+void func(std::initializer_list<int> vals)
+{
+	std::initializer_list<int>::const_iterator val;
+	for (val = vals.begin() ; val != vals.end() ; val++)
+		std::cout << "val = " << *val << '\n';
+}
+
+int main()
+{
+	do {
+		std::cout << "\033[2J\033[H";
+		func({10, 200, 3000});
+	} while (getchar() != 'q');
+
+	return 0;
+}
