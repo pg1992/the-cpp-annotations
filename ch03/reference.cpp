f844904c9fefb3a2b01af10b454a2844ab1444ca
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-11-01 03:07:57 -0300

    Show examples of refence in C++
    int& is a reference to an int variable.

--- ch03/reference.cpp
+++ ch03/reference.cpp
@@ -4,6 +4,18 @@
  */
 #include <iostream>
 
+void increase(int *valp) // expects a pointer
+{
+    std::cout << "Passed as pointer..." << std::endl;
+    *valp += 5;
+}
+
+void increase(int &valr) // same effect of the above function
+{
+    std::cout << "Passed as reference..." << std::endl;
+    valr += 5;
+}
+
 int main()
 {
     int val = 5;
@@ -14,6 +26,13 @@ int main()
 
     std::cout << "val = " << val << "\n"
                  "ref = " << ref << '\n';
+
+    increase(&val); // passing the address of val: calls increase(int*)
+    std::cout << "val = " << val << std::endl;
+
+    increase(val); // passing val as reference: calls increase(int&)...
+                   // (suggests that a copy is passed)
+    std::cout << "val = " << val << std::endl;
 
     return 0;
 }
