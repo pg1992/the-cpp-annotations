ec7836731a96385247826514a6ce7db9b14f30ef
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2022-11-03 13:22:03 -0300

    rvalue_reference: anon is passed as rval ref arguments

--- ch03/rvalue_reference.cpp
+++ ch03/rvalue_reference.cpp
@@ -5,13 +5,34 @@ int intVal()
     return 5; // anonymous variable: assignment requires a copy
 }
 
+void receive(int &value) // receive a named var
+{
+    std::cout << "int value parameter\n";
+}
+
+void receive(int const &value) // never called...
+{
+    std::cout << "int const ref parameter\n";
+}
+
+void receive(int &&value)   // receive an anon var
+{
+    ++value; // it stores in a variable  internally...
+    std::cout << "int R-value parameter, now: " << value << '\n';
+}
+
 int main()
 {
     // int &ir = intVal();      // fails: refers to a temporary
     int const &ic = intVal();   // OK: immutable temporary
     // int *ip = &intVal();     // fails: no lvalue available
 
-    // conclusion: it is not possible to modify the temporary returned to by intVal
+    // conclusion: it is not possible to modify the temporary returned to by intVal()
+
+    receive(18);        // temp: calls 'receive(int&&)'
+    int value = 5;
+    receive(value);     // lvalue: calls 'receive(int&)'
+    receive(intVal());  // anon var returned: calls 'receive(int&&)'
 
     return 0;
 }
